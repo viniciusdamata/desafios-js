4ca4bd5f244a4cd711b6e8e3621cc48176ec0e96
Vinicius Lima <viniciuslimadamata@gmail.com>
2019-02-22 17:20:20 -0400

    correção de bugs 22-02

--- cadastro pesquisa/main.js
+++ cadastro pesquisa/main.js
@@ -1,19 +1,19 @@
 let nomes = new Array();
 
 
-function addPessoas(){
+function addPessoas() {
     /**
      * Obtem o nome por id e salva seu value em uma variavel
      * Adiciona o em um vetor de Strings
      */
-    
+
     let nome = document.getElementById("nome").value;
     nomes.push(nome);
-    document.getElementById("nome").value="";
+    document.getElementById("nome").value = "";
     alert("Cadastro realizado com sucesso! ");
     console.log(nomes);
 }
-function busca(){
+function busca() {
     /**
      * Obtem o nome a ser buscado por id e salva seu value em uma variavel
      * Escreve um innerText em uma Lista
@@ -22,12 +22,17 @@ function busca(){
      * Adiciona o li criado como child da lista
      */
     let nomeBusca = document.getElementById("busca").value;
-    document.getElementById("resultado").innerText="Resultado da Busca:";
-    for(let i = 0; i < nomes.length; i++){
-        if(nomes[i].toLowerCase().startsWith(nomeBusca.toLowerCase())){
+
+    document.getElementById("resultado").innerText = "Resultado da Busca:";
+    for (let i = 0; i < nomes.length; i++) {
+        if (nomes[i].toLowerCase().startsWith(nomeBusca.toLowerCase())) {
             let elemento = document.createElement("li");
-            elemento.innerText=nomes[i];
+            elemento.innerText = nomes[i];
             document.getElementById("resultado").appendChild(elemento);
         }
     }
+
+
+
+
 }
